a6bcd257a737238bb8c27be8b554e4e68b4766ee
giovap95 <giovanni.pagnon@gmail.com>
2020-05-08 23:14:05 +0200

    Removing prints from materiale.py. Inserting minimal prints in linear.py

--- fempagno/modules/linear.py
+++ fempagno/modules/linear.py
@@ -13,39 +13,41 @@ import sys
 def linear(mesh,bcs,material_lib,parameters):
     #%% Allocating memory
     K = np.zeros(shape=(mesh.totdofs,mesh.totdofs))
-    
+
     F = np.zeros(shape=(mesh.totdofs))
-    
+
     # rotation matrices
     T = np.zeros((4,4))
-    
-    
+
+
     #%% Magic happens
-    
+print('------ Stiffness matrix assembly -------')
     for i in np.arange(mesh.elements):
-        
+
         mesh.el_type(i)
-        
+
         k = motoreFEM.stiffness_matrix(mesh,material_lib,parameters,T,i)
-        
+
         dof = motoreFEM.locglobmap(mesh,i)
 
         K = motoreFEM.assembly(k,dof,K)
-        
+
+print('------ Applying boundary conditions ------')
     #%% Applying boundary conditions
     bcs.apply_bcs(F,K,mesh)
-    
+
     #%% Solving
     #U = np.matmul(inv(K), F)
-    
+
     if np.linalg.det(K)==0:
         print('\n','\n')
         print('K is singular! the geometry is not constrained. Aborting...')
         print('\n','\n')
         sys.exit()
-    
+
+print('------ Computing displacements ------')    
     U = inv(K)@F
-    
+
     print('###########################################################')
     print('U: ',U)
     ax1 = plt.plot(mesh.cds_table[:,0],mesh.cds_table[:,1],'-o')
@@ -54,6 +56,6 @@ def linear(mesh,bcs,material_lib,parameters):
     ax2 = plt.plot(m[:,0],m[:,1],'-o')
     plt.xlim(0,2)
     plt.ylim(-1.5,2)
-    
+
     plt.show()
     return U,K
--- fempagno/modules/materiale.py
+++ fempagno/modules/materiale.py
@@ -7,22 +7,22 @@ Created on Fri Mar 27 18:59:46 2020
 import sys
 
 def elastic_properties(elMat,material_lib,i):
-    
+
     key = elMat
 
     if material_lib[key]['element'] == 'spring':
-        springStiffness =material_lib[key]['elastic properties']['stiffness']            
+        springStiffness =material_lib[key]['elastic properties']['stiffness']
         return springStiffness
-    
-    elif material_lib[key]['element'] == 'bar':            
-        E = material_lib[key]['elastic properties']["Young's modulus"]            
+
+    elif material_lib[key]['element'] == 'bar':
+        E = material_lib[key]['elastic properties']["Young's modulus"]
         return E
-    
+
     elif material_lib[key]['element'] == 'triangle':
         E = material_lib[key]['elastic properties']["Young's modulus"]
         ni = material_lib[key]['elastic properties']['Poisson ratio']
         return E,ni
-    
+
     elif material_lib[key]['element'] == 'quad':
         E = material_lib[key]['elastic properties']["Young's modulus"]
         ni = material_lib[key]['elastic properties']['Poisson ratio']
@@ -33,17 +33,17 @@ def elastic_properties(elMat,material_lib,i):
 
 
 def geometric_properties(elMat,material_lib,i):
-    
+
     key = elMat
-    
-    if material_lib[key]['element'] == 'bar':            
-        A = material_lib[key]['geometric properties']['area']            
+
+    if material_lib[key]['element'] == 'bar':
+        A = material_lib[key]['geometric properties']['area']
         return A
-    
+
     elif material_lib[key]['element'] == 'triangle':
         t = material_lib[key]['geometric properties']['thickness']
         return t
-    
+
     elif material_lib[key]['element'] == 'quad':
         t = material_lib[key]['geometric properties']['thickness']
         return t
@@ -53,28 +53,19 @@ def geometric_properties(elMat,material_lib,i):
 
 
 def stiff_matrix_info(mesh,material_lib,i):
-    
+
     key = mesh.material[i]
-    
+
     element = material_lib[key]['element']
-    print('-------------------------------------------------------------')
-    print('element',i,'is a ',element)
     evaluation = material_lib[key]['stiffness matrix']['evaluation']
     print('evaluation: ',evaluation)
-    
+
     if evaluation == 'numerical integration':
         domain = material_lib[key]['stiffness matrix']['domain']
-        print('domain: ',domain)
-        
+
         rule = material_lib[key]['stiffness matrix']['rule']
-        print('rule: ',rule)
-        
+
         points = material_lib[key]['stiffness matrix']['points']
-        print('points: ',points)
         return element, evaluation, domain, rule, points
     elif evaluation == 'closed form':
         return element, evaluation, None, None, None
-        
-
-    
-    
