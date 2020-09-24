b03e5697e821bc4ee566afd675e729232a4208dd
giovap95 <giovanni.pagnon@gmail.com>
2020-09-24 14:36:44 +0200

    Creazione meshes quadrilatere

--- fempagno/h-convergence.py
+++ fempagno/h-convergence.py
@@ -9,17 +9,25 @@ import numpy as np
 from input import inputfunction 
 
 Lnumber = [20, 10, 5, 2]
-error_vec = []
-element_vector = []
+triangle_error_vec = []
+quad_error_vec = []
+triangle_element_vector = []
+quad_element_vector = []
 
 for l in Lnumber:
     filename = 'quarter_disk' + str(l)
     error_norm, elements = inputfunction(filename)
-    error_vec.append(error_norm)
-    element_vector.append(elements)
+    triangle_error_vec.append(error_norm)
+    triangle_element_vector.append(elements)
 
+    filename = 'quarter_disk-q-' + str(l)
+    error_norm, elements = inputfunction(filename)
+    quad_error_vec.append(error_norm)
+    quad_element_vector.append(elements)
+    
+    
 m = np.log10(error_vec[3]/error_vec[2])/np.log10(element_vector[3]/element_vector[2])
-plt.loglog(element_vector, error_vec,'-o')
+plt.loglog(triangle_element_vector, triangle_error_vec,'-o', quad_element_vector, quad_error_vec)
 #plt.legend('linear triangles, slope: '+str(m))
 plt.xlabel('Number of elements')
 plt.ylabel('Error norm')
