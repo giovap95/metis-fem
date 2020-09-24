05fa7e82a597afd8a89b4a749cbabfafd50d388c
giovap95 <giovanni.pagnon@gmail.com>
2020-09-24 14:07:56 +0200

    h-convergence obtained

--- fempagno/h-convergence.py
+++ fempagno/h-convergence.py
@@ -5,6 +5,7 @@ Created on Thu Sep 24 00:56:00 2020
 @author: giova
 """
 import matplotlib.pyplot as plt
+import numpy as np
 from input import inputfunction 
 
 Lnumber = [20, 10, 5, 2]
@@ -17,7 +18,9 @@ for l in Lnumber:
     error_vec.append(error_norm)
     element_vector.append(elements)
 
+m = np.log10(error_vec[3]/error_vec[2])/np.log10(element_vector[3]/element_vector[2])
 plt.loglog(element_vector, error_vec,'-o')
+#plt.legend('linear triangles, slope: '+str(m))
 plt.xlabel('Number of elements')
 plt.ylabel('Error norm')
 plt.title('h-convergence')
